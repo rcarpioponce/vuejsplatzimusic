c011b17dc18e25ac4e9b7ea5d5f0f08c9fd7471e
Renzo R. Carpio Ponce <me@renzocarpio.com>
2017-04-22 10:27:25 -0500

    archivo de configuracion, promesas y llamada al api del lastm

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appName: 		'platzimusic',
+	apiKey: 		'd5c9fca5b7554fd534c24172a4899807',
+	secret: 		'6cea49f35f44f5a6706c0adc32a15a10',
+	registredTo: 	'rcarpioponce' 
+}
+
+//Application name	platzimusic
+//API key	d5c9fca5b7554fd534c24172a4899807
+//Shared secret	6cea49f35f44f5a6706c0adc32a15a10
+//Registered to	rcarpioponce
